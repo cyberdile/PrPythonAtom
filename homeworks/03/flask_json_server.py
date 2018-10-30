a012883d2b13bff36d89ba57aca1c61ed45839e3
cyberdile <tenstanislav@email.com>
2018-10-30 19:26:43 +0300

    Hw_03

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -8,10 +8,19 @@ def return_classifier_result(version):
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
 
+    if request.method == 'POST':
+        content = request.json
+        if version == '1':
+            data = {'version':1,'predict':content['predict']}
+        if version == '0':
+            data = {'version':0,'predict':content['old_predict']}
+        
+        return jsonify(data)
+        
 @app.route("/")
 def hello():
     #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return 'Открой терминал и вбей "python flask_server_tests.py". Если выведет "Test ok", то ты крут'
 
 if __name__ == "__main__":
     app.run()
